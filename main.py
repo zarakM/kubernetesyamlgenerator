9cbd2002ad77e818c586d91e670b7e715370fc75
Muhammad.Zarak <muhammadk@plasmacomp.com>
2025-12-09 18:23:55 +0500

    moved to openai newer version syntax

--- main.py
+++ main.py
@@ -1,20 +1,19 @@
 
 from langchain_openai import ChatOpenAI
-import openai
+from openai import OpenAI
+
+client = OpenAI(api_key=os.environ['OPENAI_API_KEY'])
 import os
 
-openai.api_key = os.environ['OPENAI_API_KEY']
+llm_model = "gpt-3.5-turbo"
 chat = ChatOpenAI(temperature=0.0, model=llm_model)
-
 
 def get_completion(prompt, model=llm_model):
     messages = [{"role": "user", "content": prompt}]
-    response = openai.ChatCompletion.create(
-        model=model,
-        messages=messages,
-        temperature=0, 
-    )
-    return response.choices[0].message["content"]
+    response = client.chat.completions.create(model=model,
+    messages=messages,
+    temperature=0)
+    return response.choices[0].message.content
 
 get_completion("What is 1+1?")
 
